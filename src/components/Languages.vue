832f0316ca6d0617c16cc6989d10d84e96b2d91a
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-05-17 19:04:50 +0200

    feat: all languages

--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -1,18 +1,18 @@
 <template>
-  <div> 
-      <LanguageElement lang="C++"/>
+  <div>
+    <LanguageElement v-for="(lang, index) in langs" v-bind:index="index" v-bind:lang="lang" v-bind:key="lang"/>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Options } from "vue-class-component";
 import LanguageElement from "./LanguageElement.vue";
-import {langs} from "../stat";
+import { langs } from "../stat";
 @Options({
-    components: {
-        LanguageElement
-    }
+  components: {
+    LanguageElement,
+  },
 })
 export default class Languages extends Vue {
-    readonly langs = langs;
+  readonly langs = langs;
 }
 </script>
